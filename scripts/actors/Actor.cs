a536259104a3d74ff9e342dab69f7dc7118018c4
Maddi <odeaminsk@gmail.com>
2024-05-02 11:42:42 -0600

    messed with shit tee hee lol idk

--- scripts/actors/Actor.cs
+++ scripts/actors/Actor.cs
@@ -6,37 +6,38 @@ public partial class Actor : CharacterBody3D
 {
 	
 	public float health = 5;
-    [Export]
-    public float maxHealth = 5;
-    public bool isDead = false;
+	[Export]
+	public float maxHealth = 5;
+	public bool isDead = false;
 
 	public float gravity = -ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-    public override void _Ready() {
+	public override void _Ready() {
 
-        health = maxHealth;
+		health = maxHealth;
 
-    }
+	}
 
-    public virtual void OnHit(float damage, Vector3 hitPoint, Vector3 hitNormal, Node3D source) 
-    {
+	public virtual void OnHit(float damage, Vector3 hitPoint, Vector3 hitNormal, Node3D source) 
+	{
 
-        health -= damage;
-        if (health <= 0 && !isDead) 
-        {
-            OnDeath();
-        }
+		health -= damage;
+		if (health <= 0 && !isDead) 
+		{
+			OnDeath();
+		}
 
-    }
+	}
 
-    public virtual void OnDeath() 
-    {
+	public virtual void OnDeath() 
+	{
 
-        isDead = true;
+		isDead = true;
 
-        //This is where Death code would go.
-        //IF I HAD SOME.
-
-    }
-    
+		//This is where Death code would go.
+		//IF I HAD SOME.
+		
+		//meowmeowmeowmeowmeowmomeowmeowmeowmeowmeowmeo >w<
+	}
+	
 }
