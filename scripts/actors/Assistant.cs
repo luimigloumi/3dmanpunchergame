c91f66e83f4430ddf04355e20bab133a440754be
luimigloumi <168661337+luimigloumi@users.noreply.github.com>
2024-05-10 11:32:23 -0600

    Update Assistant.cs

--- scripts/actors/Assistant.cs
+++ scripts/actors/Assistant.cs
@@ -26,14 +26,7 @@ public partial class Assistant : Enemy
 
 			case AssistantState.Attacking:
 
-				Transform3D trans2 = GlobalTransform.LookingAt(new(player.GlobalPosition.X, GlobalPosition.Y, player.GlobalPosition.Z), Vector3.Up);
-
-				Transform3D rotatedTrans2 = GlobalTransform;
-
-				if (rotatedTrans2.Basis.X != trans2.Basis.X) rotatedTrans2.Basis.X = rotatedTrans2.Basis.X.Normalized().Slerp(trans2.Basis.X.Normalized(), 0.2f);
-				if (rotatedTrans2.Basis.Z != trans2.Basis.Z) rotatedTrans2.Basis.Z = rotatedTrans2.Basis.Z.Normalized().Slerp(trans2.Basis.Z.Normalized(), 0.2f);
-
-				GlobalTransform = rotatedTrans2;
+				LookAt(new(player.GlobalPosition.X, GlobalPosition.Y, player.GlobalPosition.Z), Vector3.Up);
 
 				if (GlobalPosition.DistanceTo(player.GlobalPosition) > desiredDistance) {
 
@@ -50,14 +43,7 @@ public partial class Assistant : Enemy
 
 				if (!(Velocity.IsEqualApprox(Vector3.Zero))) 
 				{
-					Transform3D trans = GlobalTransform.LookingAt(GlobalPosition + new Vector3(Velocity.X, 0f, Velocity.Z), Vector3.Up);
-
-					Transform3D rotatedTrans = GlobalTransform;
-
-					if (rotatedTrans.Basis.X != trans.Basis.X) rotatedTrans.Basis.X = rotatedTrans.Basis.X.Slerp(trans.Basis.X, 0.2f);
-					if (rotatedTrans.Basis.Z != trans.Basis.Z) rotatedTrans.Basis.Z = rotatedTrans.Basis.Z.Slerp(trans.Basis.Z, 0.2f);
-
-					GlobalTransform = rotatedTrans;
+					LookAt(GlobalPosition + new Vector3(Velocity.X, 0, Velocity.Z), Vector3.Up);
 				}
 
 				Vector3 direction = (navAgent.GetNextPathPosition() - GlobalPosition).Normalized();
